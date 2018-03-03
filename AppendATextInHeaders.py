8ab89fdc86061d93453e0cabbc58e8874c2f1afd
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-03-03 09:18:00 +0530

    Minor change

--- AppendATextInHeaders.py
+++ AppendATextInHeaders.py
@@ -1,6 +1,7 @@
 #Prerequisite: Dump the list of headers that you want to modify in a text file
 #I wanted to append Microsoft's banned.h in every header of our source code
 #I had to append the #include at the end of the file and this script saved me a lot of time.
+
 import os
 headerList = open("headersToModify.txt","w")
 for root,dirs,files in os.walk("."):
