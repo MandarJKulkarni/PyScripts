28c8944a83cf97fffa74fdaea1140c5095237a11
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-02 20:33:10 +0530

    Added try block

--- NtlmAuthentication.py
+++ NtlmAuthentication.py
@@ -1,6 +1,3 @@
-#Demo for using requests package to call REST APIs
-#Usage of requests_ntlm package for windows authentication
-
 import requests
 import requests_ntlm
 
@@ -11,6 +8,8 @@ payload = '''
 "comments":"temporary",
 "AccessLevel":"basic"
 }'''
-
-getResponse =  requests.get(urlToConnect,auth=requests_ntlm.HttpNtlmAuth('winuser','winpassword'))
-postResponse = requests.post(urlToConnect,data=payload,auth=requests_ntlm.HttpNtlmAuth('winusername','winpassword'))
+try:
+    getResponse =  requests.get(urlToConnect,auth=requests_ntlm.HttpNtlmAuth('winuser','winpassword'))
+    postResponse = requests.post(urlToConnect,data=payload,auth=requests_ntlm.HttpNtlmAuth('winusername','winpassword'))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
