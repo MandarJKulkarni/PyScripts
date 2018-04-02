8313731e551d2c38ebec7349508dd99afc37682c
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-02 20:40:31 +0530

    Added a status_code check

--- NtlmAuthentication.py
+++ NtlmAuthentication.py
@@ -11,5 +11,7 @@ payload = '''
 try:
     getResponse =  requests.get(urlToConnect,auth=requests_ntlm.HttpNtlmAuth('winuser','winpassword'))
     postResponse = requests.post(urlToConnect,data=payload,auth=requests_ntlm.HttpNtlmAuth('winusername','winpassword'))
+    if postResponse.status_code == 200:
+        print ("post request succeeded")
 except:
-    print("Unexpected error:", sys.exc_info()[0])
+    print("Unexpected error:", sys.exc_info()[1])
