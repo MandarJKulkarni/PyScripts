be0e22dab4a30cc7c3f47ccccf7d953e9c06cf35
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-04-04 09:16:38 +0530

    Fixed a bug

--- uploadFileTOGCPBucket.py
+++ uploadFileTOGCPBucket.py
@@ -1,6 +1,3 @@
-#Prerequisite is user should be logged in to Google project
-#and authenticated (either through gcloud init or gcloud auth application-default login)
-
 import google.cloud
 from google.cloud import storage
 
@@ -11,4 +8,4 @@ if storageClient is not None:
     if testBucket is not None:
         testBlob = testBucket.blob("mydata.json")
         if testBlob is not None:
-            testBlob.upload_from_filename(jsonFileName)
+            testBlob.upload_from_filename("mydata.json")
