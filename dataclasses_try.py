2de761804d12b85991edd5ad6d54f68b7dad13ff
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2018-11-08 17:22:24 +0530

    added frozen parameter example

--- dataclasses_try.py
+++ dataclasses_try.py
@@ -33,9 +33,17 @@ std1.subjects = ['Hindi', 'English']
 if std1 == std2:
     print('2 students are equal')
 
+#use frozen parameter to make an immutable data class
 @dataclass(frozen=True)
 class Marks:
     maths: int
     english: int
     science: int
     hindi: int
+
+
+m_vijay = Marks(30,40,50,60)
+#throws dataclasses.FrozenInstanceError: cannot assign to field 'english'
+#m_vijay.english=55
+
+print(m_vijay)
