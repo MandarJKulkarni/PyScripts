0c392d8b67aedc09801e0a3db94818b824bc96b4
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-03-21 13:25:47 +0530

    basic example to create and await tasks

---
+++ try_asyncio.py
@@ -0,0 +1,35 @@
+import asyncio
+
+
+async def print_hello():
+    print("hello")
+
+
+async def print_world():
+    await asyncio.sleep(1)
+    print("world")
+
+
+async def print_from():
+    await asyncio.sleep(2)
+    print("from")
+
+
+async def print_mandar():
+    await asyncio.sleep(3)
+    print("Mandar")
+
+
+async def main():
+    task_list = list()
+    task_list.append(asyncio.create_task(print_mandar()))
+    task_list.append(asyncio.create_task(print_from()))
+    task_list.append(asyncio.create_task(print_world()))
+    task_list.append(asyncio.create_task(print_hello()))
+
+    # prints "hello world from Mandar", regardless of the sequence in list
+    for item in task_list:
+        await item
+
+if __name__ == '__main__':
+    asyncio.run(main())
