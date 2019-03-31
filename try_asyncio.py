ed757c1d44819fc5d066459e865d08821ba51107
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-03-31 23:46:22 +0530

    added a comment

--- try_asyncio.py
+++ try_asyncio.py
@@ -32,5 +32,6 @@ async def main():
         await item
 
 if __name__ == '__main__':
+    # asyncio.run can be used only with Python3.7
     # asyncio.run(main())
     asyncio.get_event_loop().run_until_complete(asyncio.gather(main()))
