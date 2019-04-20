3e42f364c1aa8c264a2cdeabdeb6ca5e7062db23
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-04-20 23:28:38 +0530

    added a comment

--- aiohttp_try.py
+++ aiohttp_try.py
@@ -13,6 +13,8 @@ async def main():
     async with aiohttp.ClientSession() as session:
         future1 = asyncio.ensure_future(send_request_get_response('http://google.com', session))
         future2 = asyncio.ensure_future(send_request_get_response('http://httpbin.org/get', session))
+        # the sequence of responses is async
+        # i.e. different from the sync responses first 4 from google and then 4 responses from httpbin
         await future1
         await future2
 
