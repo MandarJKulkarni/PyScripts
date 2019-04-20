be80a0a7edba23b7644542d49d6dc316d9892fba
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-04-20 23:11:01 +0530

    remove commented code

--- aiohttp_try.py
+++ aiohttp_try.py
@@ -10,13 +10,11 @@ async def send_request_get_response(url, session):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        # lp = asyncio.get_event_loop()
         future1 = asyncio.ensure_future(send_request_get_response('http://google.com', session))
         future2 = asyncio.ensure_future(send_request_get_response('http://httpbin.org/get', session))
         await future1
         await future2
-    # for j in range(1, 5):
-    #     asyncio.run(send_request_get_response('http://httpbin.org/get'))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
