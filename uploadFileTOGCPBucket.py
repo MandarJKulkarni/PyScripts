b4e1fb8c5537c0e4217c72c0a94541c1fe2bf841
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2022-09-26 07:07:59 +0530

    Minor change

--- uploadFileTOGCPBucket.py
+++ uploadFileTOGCPBucket.py
@@ -6,7 +6,7 @@ from google.cloud import storage
 
 storageClient = storage.Client()
 if storageClient is not None:
-    bucketName = input("Enter the bucket name to upload your file:")
+    bucketName = input("Enter the unique bucket name to upload your file:")
     testBucket = storageClient.get_bucket(bucketName)
     if testBucket is not None:
         testBlob = testBucket.blob("mydata.json")
